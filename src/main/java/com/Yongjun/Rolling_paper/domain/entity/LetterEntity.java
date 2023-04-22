ea59684f59f7435ce8c0d6fb7cd5bba8eecd09fa
hong <kevin0928@naver.com>
2023-04-22 19:21:01 +0900

    sql_letter_entity

--- src/main/java/com/Yongjun/Rolling_paper/domain/entity/LetterEntity.java
+++ src/main/java/com/Yongjun/Rolling_paper/domain/entity/LetterEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Table(name = "Letter")
+@Table(name = "Letter_Entity")
 public class LetterEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
